6a9f2b8a10a54d6002ccda9aca1f8fc9c71b363e
Mohanraj <Mohanraj@Madhanraj>
2025-10-09 15:33:59 +0530

    dayone to dayseven

---
+++ corejavaprograms/src/com/tnsif/dayfour/secondpackage/Executor.java
@@ -0,0 +1,19 @@
+//Program to demonstrate accessing classes and methods of different package
+package com.tnsif.dayfour.secondpackage;
+
+import com.tnsif.dayfour.firstpackage.Base;
+
+public class Executor {
+	public static void main(String[] args) {
+		// accessing different package class
+		Base b1 = new Base();
+
+
+
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
+	}
+}
+
+
---
+++ corejavaprograms/src/day2/forLoop_example.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class forLoop_example {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     for(int a=0;a<15;a++) {
+    	System.out.println(a);
+     }
+	}
+
+}
